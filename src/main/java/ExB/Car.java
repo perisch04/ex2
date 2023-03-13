363e5f85f39b86a05a304b91644a4d7b76c19f03
perik <periklarasxa@gmail.com>
2023-03-13 22:41:38 +0200

    Ex2

---
+++ src/main/java/ExB/Car.java
@@ -0,0 +1,82 @@
+package ExB;
+import java.util.Scanner;
+public class Car {
+    Scanner in = new Scanner(System.in);
+    private String manufacturer;
+    private String model;
+    private int year;
+    private int cc;
+    private int km;
+    private int lastService;
+    private int nextService;
+    private boolean serviceStatus;
+
+    public int getKm(){
+        return km;
+    }
+    public int getNextService(){
+        return nextService;
+    }
+    public boolean needsService(){ //checks if the car needs service
+        if(km >= nextService){
+            return false;
+        }
+        return true;
+    }
+
+    public Car(){ //default constructor sets all values to 0 or "" and serviceStatus to false
+        manufacturer = " ";
+        model = " ";
+        year = 0;
+        cc = 0;
+        km = 0;
+        lastService = 0;
+        nextService = 0;
+        serviceStatus = false;
+    }
+    //constructor with parameters sets all values to the parameters and serviceStatus from needsService()
+    public Car(String manufacturer, String model, int year, int cc, int km, int lastService, int nextService){
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+        this.cc = cc;
+        this.km = km;
+        this.lastService = lastService;
+        this.nextService = nextService;
+        this.serviceStatus = needsService();
+    }
+   
+    public void read(){ //reads all values from the user and sets serviceStatus from needsService()
+        System.out.print("Manufacturer: ");
+        manufacturer = in.nextLine();
+        System.out.print("Model: ");
+        model = in.nextLine();
+        System.out.print("Year: ");
+        year = in.nextInt();
+        System.out.print("CC: ");
+        cc = in.nextInt();
+        System.out.print("KM: ");
+        km = in.nextInt();
+        System.out.print("Last Service: ");
+        lastService = in.nextInt();
+        System.out.print("Next Service: ");
+        nextService = in.nextInt();
+        serviceStatus = needsService();
+    }
+
+    public void print(){ //prints all values
+        System.out.println("Manufacturer:"+manufacturer);
+        System.out.println("Model:"+model);
+        System.out.println("Year:"+year);
+        System.out.println("CC:"+cc);
+        System.out.println("KM:"+km);
+        System.out.println("Last Service:"+lastService);
+        System.out.println("Next Service:"+nextService);
+        if (serviceStatus){
+            System.out.println("Service Status: Does not need service");
+        }
+        else{
+            System.out.println("Service Status: Needs service");
+        }
+    }
+}
---
+++ src/main/java/ExB/Main.java
@@ -0,0 +1,34 @@
+package ExB;
+public class Main {
+    public static void main(String[] args) {
+       final int MAX_CARS = 10; //max number of cars
+       int km = 0; //km due
+       int id = 0; //id of the car that needs service the most
+       Car car1 = new Car(); //default constructor
+       Car car2 = new Car(); //default constructor but with values from read()
+       Car car3 = new Car("Volvo","S70",1998,2000,350000,320000,360000); //constructor with parameters
+       car2.read(); //reads values from the user
+       car1.print(); //prints all values
+       car2.print();
+       car3.print();
+       Car[] Honda = new Car[MAX_CARS]; //array of cars
+       for(int i=0;i<MAX_CARS;i++){ //reads values from the user and sets them to the array
+           Honda[i] = new Car();
+           Honda[i].read();
+       }
+       for(int i=0;i<MAX_CARS;i++){ //prints all values from the array
+           Honda[i].print();
+       }
+       for(int i=0;i<MAX_CARS;i++){ //checks if the car needs service and prints the car that needs service the most and how many km its due
+           if(!Honda[i].needsService()){
+            if(Honda[i].getKm()-Honda[i].getNextService()>km){
+                km = Honda[i].getKm()-Honda[i].getNextService();
+                id = i;
+                }
+           }
+       }
+       System.out.println("Printing the car that needs service the most:");
+       Honda[id].print();
+       System.out.println("Km due: "+km+"km");
+    }
+}
